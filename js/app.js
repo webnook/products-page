b8b5be9f54a2b1725a9d3a7b2a1fa9ce6cd45728
Aryan Rajabi <aryanrajabi1990@gmail.com>
2024-04-21 23:10:58 +0330

    filter products base on product-name created

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const searchInput = document.getElementById("search-input");
+const products = document.querySelectorAll(".products__item");
+
+const searchHandler = (event) => {
+  const searchValue = event.target.value.toLowerCase().trim();
+  products.forEach((product) => {
+    const productName = product.children[1].innerText.toLowerCase();
+    if (productName.includes(searchValue)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+};
+
+searchInput.addEventListener("keyup", searchHandler);
+ 
